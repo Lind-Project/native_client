3b21918d9bee8ac8cc8b69207bd562b5e036d1bb
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2019-11-01 13:01:11 -0400

    flags checking

--- src/trusted/service_runtime/nacl_syscall_common.c
+++ src/trusted/service_runtime/nacl_syscall_common.c
@@ -495,7 +495,7 @@ int32_t NaClSysDup(struct NaClAppThread *natp, int oldfd) {
   }
 
   new_hd->d = lind_dup(old_hd->d, nap->cage_id);
-  new_hd->flags = old_hd->flags;
+  // new_hd->flags = old_hd->flags;
   new_hd->cageid = nap->cage_id;
 
   /* Set new nacl desc as available */
@@ -571,7 +571,7 @@ int32_t NaClSysDup2(struct NaClAppThread  *natp,
     struct NaClHostDesc *new_hd = new_self->hd;
 
     new_hd->d = lind_dup2(old_hd->d, new_hd->d, nap->cage_id);
-    new_hd->flags = old_hd->flags;
+    // new_hd->flags = old_hd->flags;
     new_hd->cageid = nap->cage_id;
 
     /* Re-add the nacl desc to the nap */
@@ -589,7 +589,7 @@ int32_t NaClSysDup2(struct NaClAppThread  *natp,
     /* We have to use regular dup at this point because were creating a lind FD from scratch */
 
     new_hd->d = lind_dup(old_hd->d, nap->cage_id);
-    new_hd->flags = old_hd->flags;
+    // new_hd->flags = old_hd->flags;
     new_hd->cageid = nap->cage_id;
 
     /* Set new nacl desc as available */
