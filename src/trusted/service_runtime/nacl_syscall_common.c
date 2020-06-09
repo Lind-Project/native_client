981df6d84232c64737cafc2cb6fdbc03618f50e7
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2020-06-09 17:32:06 -0400

    baseline

--- src/trusted/service_runtime/nacl_syscall_common.c
+++ src/trusted/service_runtime/nacl_syscall_common.c
@@ -1039,9 +1039,13 @@ int32_t NaClSysWrite(struct NaClAppThread *natp,
   NaClVmIoWillStart(nap,
                     (uint32_t)(uintptr_t)buf,
                     (uint32_t)(((uintptr_t)buf) + count - 1));
+
+  printf("writing count: %d\n", count);
   if (fd == 1) write_result = count;
   else write_result = ((struct NaClDescVtbl const *)ndp->base.vtbl)->Write(ndp, (void *)sysaddr, count);
 
+  printf("write result: %d", write_result);
+  
   NaClVmIoHasEnded(nap,
                    (uint32_t)(uintptr_t)buf,
                    (uint32_t)(((uintptr_t)buf) + count - 1));
