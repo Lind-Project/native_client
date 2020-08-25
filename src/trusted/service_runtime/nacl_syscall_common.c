772b705ffd5125a2f4cb2453122803cf1ff729a5
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2020-08-25 13:51:53 -0400

    dtor

--- src/trusted/service_runtime/nacl_syscall_common.c
+++ src/trusted/service_runtime/nacl_syscall_common.c
@@ -4055,17 +4055,20 @@ int32_t NaClSysFork(struct NaClAppThread *natp) {
   int ret = -NACL_ABI_ENOMEM;
 
   NaClLog(1, "%s\n", "[NaClSysFork] NaCl fork starts!");
+  
+  NaClXMutexLock(&nap->mu); 
 
   /* set up new "child" NaClApp */
   NaClLogThreadContext(natp);
   nap_child = NaClChildNapCtor(natp->nap);
+  NaClXMutexLock(&nap_child->mu); 
+
   child_argc = nap_child->argc;
   child_argv = nap_child->argv;
   nap_child->running = 0;
   ret = nap_child->cage_id;
 
-  NaClXMutexLock(&nap->mu); 
-  NaClXMutexLock(&nap_child->mu); 
+
 
   lind_fork(ret, nap->cage_id);
   
