e009dabba5ddfcfe80051b672bc431a0b093f31e
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2020-12-10 22:08:02 -0500

    nopipe

--- src/shared/platform/lind_platform.c
+++ src/shared/platform/lind_platform.c
@@ -409,7 +409,7 @@ int lind_close (int fd, int cageid)
 }
 
 int counter = 0;
-long long PIPE_AMOUNT = 1UL << 27;
+long long PIPE_AMOUNT = 1UL << 26;
 int end = 0;
 
 int lind_read (int fd, int size, void *buf, int cageid)
