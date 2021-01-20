b779771764de2ee484c5fb066b714eb94e28f6f9
kliu513 <62267195+kliu513@users.noreply.github.com>
2021-01-21 02:24:26 +0800

    Fix

--- src/shared/platform/lind_platform.h
+++ src/shared/platform/lind_platform.h
@@ -47,6 +47,7 @@
 #define LIND_safe_fs_write              13
 #define LIND_safe_fs_lseek              14
 #define LIND_fs_ioctl                   15
+#define LIND_safe_fs_getcwd             16
 #define LIND_safe_fs_fxstat             17
 #define LIND_safe_fs_fstatfs            19
 #define LIND_safe_fs_mmap               21
