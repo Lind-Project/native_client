d7b85108d88d95c86f72bc8c337182fdc58fa123
Çağlar Doğan <dogancaglar05@gmail.com>
2021-05-27 08:20:29 +0300

    Added syscall implementation of gethostname

--- src/trusted/service_runtime/nacl_syscall_common.h
+++ src/trusted/service_runtime/nacl_syscall_common.h
@@ -334,6 +334,9 @@ int32_t NaClSysWait(struct NaClAppThread *natp, uint32_t *stat_loc);
 int32_t NaClSysWait4(struct NaClAppThread *natp, int pid, uint32_t *stat_loc, int options, void *rusage);
 int32_t NaClSysSigProcMask(struct NaClAppThread *natp, int how, const void *set, void *oldset);
 
+int32_t NaClSysGethostname(char *name, size_t len);
+
+
 EXTERN_C_END
 
 #endif  /* NATIVE_CLIENT_SERVICE_RUNTIME_NACL_SYSCALL_COMMON_H__ */
