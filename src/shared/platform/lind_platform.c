4951c90f27b046b840f3e3eaccfd8e30134da306
Çağlar Doğan <dogancaglar05@gmail.com>
2021-05-27 19:53:54 +0300

    Update lind_platform.c

--- src/shared/platform/lind_platform.c
+++ src/shared/platform/lind_platform.c
@@ -790,7 +790,7 @@ void lind_exit(int status, int cageid)
 int lind_gethostname (char *name, size_t len, int cageid)
 {
     LIND_API_PART1;
-    callArgs = Py_BuildValue("(s[ii])", LIND_safe_net_gethostname, len, cageid);
+    callArgs = Py_BuildValue("(i[ii])", LIND_safe_net_gethostname, len, cageid);
     LIND_API_PART2;
     COPY_DATA(name, len)
     LIND_API_PART3;
