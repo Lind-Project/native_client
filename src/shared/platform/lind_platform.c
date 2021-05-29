b77ba060a4abcd384492ee7e57171c87b6f8b861
Çağlar Doğan <dogancaglar05@gmail.com>
2021-05-29 11:22:21 +0300

    Update lind_platform.c

--- src/shared/platform/lind_platform.c
+++ src/shared/platform/lind_platform.c
@@ -480,14 +480,6 @@ int lind_fcntl_set (int fd, int cmd, long set_op)
     LIND_API_PART3;
 }
 
-int lind_socket (int domain, int type, int protocol)
-{
-    LIND_API_PART1;
-    callArgs = Py_BuildValue("(i[iii])", LIND_safe_net_socket, domain, type, protocol);
-    LIND_API_PART2;
-    LIND_API_PART3;
-}
-
 int lind_bind (int sockfd, socklen_t addrlen, const struct sockaddr *addr)
 {
     LIND_API_PART1;
