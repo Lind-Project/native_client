a186913547bb390e2f3ef0797d89ccef196e4748
jesings <34111484+jesings@users.noreply.github.com>
2021-08-18 10:21:40 -1000

    Switched path for fcntl, fixed dupfd, fixed lseek improper return value

--- src/trusted/service_runtime/sel_ldr.h
+++ src/trusted/service_runtime/sel_ldr.h
@@ -927,6 +927,7 @@ void InitializeCage(struct NaClApp *nap, int cage_id);
 
 /* Find the next usuable fd */
 int NextFd(int cage_id);
+int NextFdBounded(int cage_id, int lowerbound);
 
 static INLINE void NaClLogUserMemoryContent(struct NaClApp *nap, uintptr_t user_addr) {
   char *addr = (char *)NaClUserToSys(nap, user_addr);
