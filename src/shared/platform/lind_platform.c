db2d63010d3ffdcaef9b7f3451a1707a134d2916
aKpkc <11726205+kapkic@users.noreply.github.com>
2021-09-02 16:39:57 -0400

    Fix-test

--- src/shared/platform/lind_platform.c
+++ src/shared/platform/lind_platform.c
@@ -551,7 +551,7 @@ int lind_getpeername (int sockfd, socklen_t addrlen_in, __SOCKADDR_ARG addr, soc
     LIND_API_PART1;
     callArgs = Py_BuildValue("(i[iii])", LIND_safe_net_getpeername, sockfd, addrlen_in, cageid);
     LIND_API_PART2;	
-	COPY_DATA_OFFSET(addr, sizeof(*addr), 2, 0)
+	COPY_DATA_OFFSET(addrlen_in, sizeof(*addrlen_in), 2, 0)
     if (addrlen_out)
 	{
         *addrlen_out = addrlen_in;
