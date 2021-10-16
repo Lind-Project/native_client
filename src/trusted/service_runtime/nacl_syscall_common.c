4ebdd6d252d53ce14b0af30ec65f6cd8206e902b
Nicholas Renner <nr2185@nyu.edu>
2021-10-15 21:28:31 -0400

    same fix for exec (#73)

--- src/trusted/service_runtime/nacl_syscall_common.c
+++ src/trusted/service_runtime/nacl_syscall_common.c
@@ -4240,6 +4240,13 @@ int32_t NaClSysExecve(struct NaClAppThread *natp, char const *path, char *const 
   /* initialize child from parent state */
   NaClLogThreadContext(natp);
   int child_cage_id = INIT_PROCESS_NUM + ++fork_num;
+
+  /* Copy fd table in SafePOSIX */
+  NaClXMutexLock(&nap->mu); 
+  NaClLog(2, "Copying fd table in SafePOSIX\n");
+  lind_exec(child_cage_id, nap->cage_id);
+  NaClXMutexUnlock(&nap->mu);
+
   nap_child = NaClChildNapCtor(nap, child_cage_id);
   nap_child->running = 0;
   nap_child->in_fork = 0;
@@ -4363,20 +4370,15 @@ int32_t NaClSysExecve(struct NaClAppThread *natp, char const *path, char *const 
     NaClLog(LOG_FATAL, "%s\n", "parent vmmap page NaClMprotect failed!");
   }
 
-  /* Copy fd table in SafePOSIX */
-  NaClXMutexLock(&nap->mu); 
-  NaClXMutexLock(&nap_child->mu); 
-  NaClLog(2, "Copying fd table in SafePOSIX\n");
-  lind_exec(nap_child->cage_id, nap->cage_id);
-  NaClXMutexUnlock(&nap_child->mu);
-  NaClXMutexUnlock(&nap->mu);
-
   /* execute new binary, we pass NULL as parent natp since we're not basing the new thread off of this one. */
   ret = -NACL_ABI_ENOEXEC;
   NaClLog(1, "binary = %s\n", nap->binary);
   if (!NaClCreateThread(THREAD_LAUNCH_EXEC, NULL, nap_child, child_argc, child_argv, nap_child->clean_environ)) {
     NaClLog(LOG_ERROR, "%s\n", "NaClCreateThread() failed");
     NaClEnvCleanserDtor(&env_cleanser);
+    /* remove child cage */
+    lind_exit(EXIT_FAILURE, child_cage_id);
+
     goto fail;
   }
     
