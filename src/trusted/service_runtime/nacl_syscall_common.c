6c23a8d585755742f17817eb2e57c79eae3ea815
Nicholas Renner <nicholassrenner@gmail.com>
2022-02-28 18:48:57 -0500

    change thread exit for signals

--- src/trusted/service_runtime/nacl_syscall_common.c
+++ src/trusted/service_runtime/nacl_syscall_common.c
@@ -366,7 +366,6 @@ int32_t NaClSysExit(struct NaClAppThread  *natp,
   lind_exit(status, nap->cage_id);
   NaClLog(1, "Exit syscall handler: %d\n", status);
   (void) NaClReportExitStatus(nap, NACL_ABI_W_EXITCODE(status, 0));
-  NaClThreadCancelCleanup();
   NaClAppThreadTeardown(natp);
   free((void*) nap->clean_environ);
   /* NOTREACHED */
