9becfe7661eca8b2c2060765ceca709a4f5e2e5c
Nicholas Renner <nicholassrenner@gmail.com>
2022-03-08 16:41:32 -0500

    try3

--- src/trusted/service_runtime/sel_main.c
+++ src/trusted/service_runtime/sel_main.c
@@ -256,8 +256,7 @@ int NaClSelLdrMain(int argc, char **argv) {
   
   cagecount = 0;
 
-  struct NaClApp *nap = NULL;
-  nap = NaClAlignedMalloc(sizeof(*nap), __alignof(struct NaClApp));
+  struct NaClApp *nap = (struct NaClApp*)malloc(sizeof(struct NaClApp));
 
   /* Initialize cage early on to avoid Cage 0 */
   InitializeCage(nap, 1);
