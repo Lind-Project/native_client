b524cc4b04e9c3eae9503514c55c11c94a8c85fb
Nicholas Renner <nicholassrenner@gmail.com>
2022-03-07 23:50:20 -0500

    try3

--- src/trusted/service_runtime/nacl_syscall_common.c
+++ src/trusted/service_runtime/nacl_syscall_common.c
@@ -368,6 +368,8 @@ int32_t NaClSysExit(struct NaClAppThread  *natp,
   (void) NaClReportExitStatus(nap, NACL_ABI_W_EXITCODE(status, 0));
   NaClAppThreadTeardown(natp);
   free((void*) nap->clean_environ);
+  NaClVmmapExitDtor(nap);
+
   /* NOTREACHED */
   return -NACL_ABI_EINVAL;
 
