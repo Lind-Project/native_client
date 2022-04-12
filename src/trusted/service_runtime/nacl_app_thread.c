556b9f63b0d185fc5f6d564e3de5c0f81a0d724a
Nicholas Renner <nicholassrenner@gmail.com>
2022-04-12 02:39:48 -0400

    testing nap free functions

--- src/trusted/service_runtime/nacl_app_thread.c
+++ src/trusted/service_runtime/nacl_app_thread.c
@@ -363,11 +363,13 @@ void NaClAppThreadTeardownChildren(struct NaClAppThread *natp) {
     else {
       NaClLog(1, "[NaClAppThreadTeardown][parent %d] removed cage: cage_id = %d\n", nap_parent->cage_id, nap->cage_id);
     }
+    NaClXMutexLock(&nap->children_mu);
 
     for (int i = 0; i < (&nap->children)->ptr_array_space; i++) {
       struct NaClApp* nap_child = (struct NaClApp *) DynArrayGet(&nap->children, i);
       if (nap_child) nap_child->parent = NULL;
     }
+    NaClXMutexUnlock(&nap->children_mu);
 
     NaClXCondVarBroadcast(&nap_parent->children_cv);
     NaClXMutexUnlock(&nap_parent->children_mu);
