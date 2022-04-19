208a266ba1e509aebef2f40d3c9bcb980a5f9116
Nicholas Renner <nicholassrenner@gmail.com>
2022-04-19 00:20:15 -0400

    vmmap

--- src/trusted/service_runtime/sel_ldr.c
+++ src/trusted/service_runtime/sel_ldr.c
@@ -385,7 +385,7 @@ void NaClAppDtor(struct NaClApp *nap) {
   NaClLog(3, "Deleting io regions\n");
   NaClIntervalMultisetDelete(nap->mem_io_regions);
   nap->mem_io_regions = NULL;
-  // NaClVmmapExitDtor(&nap->mem_map);
+  NaClVmmapDtor(&nap->mem_map);
 
   NaClLog(3, "Tearing down dyn arrays\n");
   DynArrayDtor(&nap->children);
--- src/trusted/service_runtime/sel_mem.h
+++ src/trusted/service_runtime/sel_mem.h
@@ -75,7 +75,6 @@ void                  NaClVmmapIterErase(struct NaClVmmapIter *nvip);
 int   NaClVmmapCtor(struct NaClVmmap  *self) NACL_WUR;
 
 void  NaClVmmapDtor(struct NaClVmmap  *self);
-void NaClVmmapExitDtor(struct NaClVmmap *self);
 
 /*
  * NaClVmmapAdd does not check whether the newly mapped region overlaps
