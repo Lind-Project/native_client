206f611d28323c64277849b720f3aaae6909c1c5
Nicholas Renner <nicholassrenner@gmail.com>
2022-06-07 00:51:39 -0400

    add zombie list and reap for wait

--- src/trusted/service_runtime/sel_ldr_standard.c
+++ src/trusted/service_runtime/sel_ldr_standard.c
@@ -646,7 +646,7 @@ int NaClReportExitStatus(struct NaClApp *nap, int exit_status) {
      */
   }
   nap->exit_status = exit_status;
-  NaClAddZombie(nap);
+  if (nap->parent) NaClAddZombie(nap);
   nap->running = 0;
   NaClXCondVarSignal(&nap->cv);
 
