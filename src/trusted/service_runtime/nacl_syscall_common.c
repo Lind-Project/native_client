99859492c672b8efc73f18cedc5fcd012c5068c6
jesings <34111484+jesings@users.noreply.github.com>
2022-09-03 12:57:57 -0400

    Modified fork to preserve shared memory mappings in child (#125)
    * Modified fork to preserve shared memory mappings in child
    
    * Fancy mremap use!
    
    * Major posix shm fixup
    
    * Sizeof fix
    
    * Update shm count on exit
    
    * Extra comments, moved shmid into separate field in vmmap entry
    
    Co-authored-by: lind <lind@nyu.edu>

--- src/trusted/service_runtime/nacl_syscall_common.c
+++ src/trusted/service_runtime/nacl_syscall_common.c
@@ -2836,8 +2836,7 @@ int32_t NaClSysShmget(struct NaClAppThread  *natp,
 
   NaClLog(2, "Entered NaClSysShmget(0x%08"NACL_PRIxPTR" , %d, %lu, %d)\n",
            (uintptr_t)natp, key, size, shmflg);
-
-
+  
   alloc_rounded_size = NaClRoundAllocPage(size);
   if (alloc_rounded_size != size) {
     NaClLog(1, "NaClSysShmget: rounded size to 0x%"NACL_PRIxS"\n",
@@ -2846,9 +2845,17 @@ int32_t NaClSysShmget(struct NaClAppThread  *natp,
 
   retval = lind_shmget(key, alloc_rounded_size, shmflg, nap->cage_id);
 
-  if ((retval > 0) && (shmflg & IPC_CREAT)) {
-    shmtable[retval].size = alloc_rounded_size;
-    shmtable[retval].rmid = false;
+  if (retval > 0) {
+    if(retval >= FILE_DESC_MAX)
+      NaClLog(LOG_FATAL, "NaClSysShmget: shmid returned by lind is too large!\n");
+    if(shmflg & IPC_CREAT) {
+      shmtable[retval].size = alloc_rounded_size;
+      shmtable[retval].rmid = false;
+      shmtable[retval].extant = true;
+    } else {
+      if(!shmtable[retval].extant)
+        NaClLog(LOG_FATAL, "NaClSysShmget: shmid returned by lind does not exist!\n");
+    }
   }
 
   return retval;
@@ -2870,6 +2877,11 @@ int32_t NaClSysShmat(struct NaClAppThread  *natp,
 
   NaClLog(2, "Entered NaClSysShmat(0x%08"NACL_PRIxPTR" , %d, %lx, %d)\n",
            (uintptr_t)natp, shmid, (uintptr_t)shmaddr, shmflg);
+
+  if((unsigned) shmid >= FILE_DESC_MAX || !shmtable[shmid].extant) {
+    NaClLog(2, "NaClSysShmat: shmid invalid\n");
+    return -NACL_ABI_EINVAL;
+  }
 
   length = shmtable[shmid].size;
   if (!length) return -NACL_ABI_EINVAL;
@@ -3012,7 +3024,7 @@ int32_t NaClSysShmat(struct NaClAppThread  *natp,
 
   if ((unsigned int) -1 == map_result) {
     NaClLog(LOG_INFO,
-            ("NaClHostDescMap: "
+            ("NaClSysShmat: "
              "mmap(0x%08"NACL_PRIxPTR", %d"NACL_PRIxS", "
              "0x%d, 0x%d)"
              " failed, errno %d.\n"),
@@ -3029,14 +3041,15 @@ int32_t NaClSysShmat(struct NaClAppThread  *natp,
   }
 
   if (length > 0) {
-    NaClVmmapAddWithOverwrite(&nap->mem_map,
-                              NaClSysToUser(nap, sysaddr) >> NACL_PAGESHIFT,
-                              length >> NACL_PAGESHIFT,
-                              prot,
-                              NACL_ABI_MAP_SHARED | NACL_ABI_MAP_FIXED,
-                              NULL,
-                              0,
-                              length);
+    NaClVmmapAddWithOverwriteAndShmid(&nap->mem_map,
+                                      NaClSysToUser(nap, sysaddr) >> NACL_PAGESHIFT,
+                                      length >> NACL_PAGESHIFT,
+                                      prot,
+                                      NACL_ABI_MAP_SHARED | NACL_ABI_MAP_FIXED,
+                                      shmid,
+                                      NULL,
+                                      0,
+                                      length);
 
     shmtable[shmid].count++;
   }
@@ -3099,7 +3112,7 @@ int32_t NaClSysShmdt(struct NaClAppThread  *natp,
   if (NaClSysCommonAddrRangeContainsExecutablePages(nap,
                                                     (uintptr_t) shmaddr,
                                                     1)) {
-    NaClLog(2, "NaClSysMunmap: region contains executable pages\n");
+    NaClLog(2, "NaClSysShmdt: region contains executable pages\n");
     retval = -NACL_ABI_EINVAL;
     goto cleanup;
   }
@@ -3111,15 +3124,16 @@ int32_t NaClSysShmdt(struct NaClAppThread  *natp,
     goto cleanup;
   }
 
-  shmtable[shmid].count--;
-  length = shmtable->size;
-  if ((shmtable[shmid].rmid) && (!shmtable[shmid].count)) {
-    clear_shmentry(shmid);
-  }
+  if((unsigned) shmid >= FILE_DESC_MAX || !shmtable[shmid].extant)
+      NaClLog(LOG_FATAL, "NaClSysShmdt: nonsense shmid returned by lind_shmdt!");
 
+  length = shmtable[shmid].size;
+
+  //When the shmid entry is freed, we decrement the shm refcount
   NaClVmmapRemove(&nap->mem_map,
                   NaClSysToUser(nap, sysaddr) >> NACL_PAGESHIFT,
                   length >> NACL_PAGESHIFT);
+  retval = 0;
 
 cleanup:
   NaClXMutexUnlock(&nap->mu);
@@ -3139,8 +3153,13 @@ int32_t NaClSysShmctl(struct NaClAppThread        *natp,
   NaClLog(2, "Entered NaClSysShmctl(0x%08"NACL_PRIxPTR" , %d, %d ,""%p"NACL_PRIxPTR")\n",
            (uintptr_t)natp, shmid, cmd, (void *) buf);
 
+  if((unsigned) shmid >= FILE_DESC_MAX || !shmtable[shmid].extant) {
+    NaClLog(2, "NaClSysShmat: shmid invalid\n");
+    return -NACL_ABI_EINVAL;
+  }
+
   if (cmd == IPC_STAT) {
-    bufsysaddr = (struct lind_shmid_ds*) NaClUserToSysAddrRangeProt(nap, (uintptr_t) buf, sizeof(bufsysaddr), NACL_ABI_PROT_READ);
+    bufsysaddr = (struct lind_shmid_ds*) NaClUserToSysAddrRangeProt(nap, (uintptr_t) buf, sizeof(*bufsysaddr), NACL_ABI_PROT_READ);
   } else bufsysaddr = NULL;
 
   if ((void*) kNaClBadAddress == bufsysaddr) {
