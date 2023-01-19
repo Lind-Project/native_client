b8e112161c9ad87ba90ca039c997a573ada59d28
Nicholas Renner <nicholassrenner@gmail.com>
2023-01-18 22:55:32 -0500

    fix statbuf fields

--- src/trusted/service_runtime/nacl_syscall_common.c
+++ src/trusted/service_runtime/nacl_syscall_common.c
@@ -1045,15 +1045,12 @@ int32_t NaClSysFstat(struct NaClAppThread *natp,
   NaClLog(2, "sizeof(struct nacl_abi_stat) = %" NACL_PRIdS " (0x%" NACL_PRIxS ")\n",
           sizeof(*nasp), sizeof(*nasp));
 
-  if (d < 0)
-    return -NACL_ABI_EBADF;
+  if (d < 0) return -NACL_ABI_EBADF;
 
   retval = lind_fxstat(d, &result, nap->cage_id);
 
-  if (!retval)
-  {
-    if (!NaClCopyOutToUser(nap, (uintptr_t)nasp, &result, sizeof(result)))
-      return -NACL_ABI_EFAULT;
+  if (!retval) {
+    if (!NaClCopyOutToUser(nap, (uintptr_t)nasp, &result, sizeof(result))) return -NACL_ABI_EFAULT;
   }
 
   return retval;
@@ -1093,41 +1090,23 @@ int32_t NaClSysLStat(struct NaClAppThread *natp,
   struct NaClApp *nap = natp->nap;
   int32_t retval = -NACL_ABI_EINVAL;
   char path[NACL_CONFIG_PATH_MAX];
-  nacl_host_stat_t stbuf;
+  struct nacl_abi_stat abi_stbuf;
 
-  NaClLog(2, "Entered NaClSysLStat(0x%08" NACL_PRIxPTR ", 0x%08" NACL_PRIxPTR ","
+  NaClLog(2, "Entered NaClSysStat(0x%08" NACL_PRIxPTR ", 0x%08" NACL_PRIxPTR ","
              " 0x%08" NACL_PRIxPTR ")\n",
           (uintptr_t)natp, (uintptr_t)pathname, (uintptr_t)buf);
 
   retval = CopyPathFromUser(nap, path, sizeof(path), (uintptr_t)pathname);
-  if (retval)
-  {
-    goto cleanup;
-  }
+  if (retval) return retval;
 
   retval = NaClStatAclCheck(nap, path);
-  if (retval)
-  {
-    goto cleanup;
+  if (retval) return retval;
+
+  retval = lind_xstat(path, &abi_stbuf, nap->cage_id);
+  if (!retval) {
+    if (!NaClCopyOutToUser(nap, (uintptr_t)buf, &abi_stbuf, sizeof(abi_stbuf))) retval = -NACL_ABI_EFAULT;
   }
 
-  /*
-   * Perform a host stat.
-   */
-  retval = lind_xstat(path, &stbuf, nap->cage_id);
-  if (!retval)
-  {
-    struct nacl_abi_stat abi_stbuf;
-
-    retval = NaClAbiStatHostDescStatXlateCtor(&abi_stbuf,
-                                              &stbuf);
-    if (!NaClCopyOutToUser(nap, (uintptr_t)buf,
-                           &abi_stbuf, sizeof(abi_stbuf)))
-    {
-      retval = -NACL_ABI_EFAULT;
-    }
-  }
-cleanup:
   return retval;
 }
 
