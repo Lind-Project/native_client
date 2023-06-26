4d31f55b94c67530ec947d63993f87ce9b884a73
jesings <34111484+jesings@users.noreply.github.com>
2023-06-26 13:46:12 +0000

    revert to fix untrusted

--- src/trusted/service_runtime/linux/nacl_signal.c
+++ src/trusted/service_runtime/linux/nacl_signal.c
@@ -273,6 +273,7 @@ static int DispatchToUntrustedHandler(struct NaClAppThread *natp,
   regs->stack_ptr = new_stack_ptr;
 #elif NACL_ARCH(NACL_BUILD_ARCH) == NACL_x86 && NACL_BUILD_SUBARCH == 64
   natp->user.rdi = context_user_addr; /* Argument 1 */
+  natp->user.prog_ctr = NaClUserToSys(nap, lind_exception_handler);
   natp->user.new_prog_ctr = NaClUserToSys(nap, lind_exception_handler);
   natp->user.rsp = NaClUserToSys(nap, new_stack_ptr);
 #elif NACL_ARCH(NACL_BUILD_ARCH) == NACL_arm
