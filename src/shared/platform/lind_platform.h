4e3c0e01613a25d4f717b1de4a6891866cf636ac
kuzeyardabulut <54737933+kuzeyardabulut@users.noreply.github.com>
2023-07-15 22:02:27 +0300

    Removing mknod syscall (#141)
    * Added Mknod and Fchdir
    
    * Update lind_platform.c
    
    * Update lind_platform.c
    
    * Update nacl_syscalls.h
    
    * Update nacl_syscalls.h
    
    * Remove mknod Syscall
    
    * Update nacl_syscalls.h
    
    * Update lind_platform.c
    
    * Update nacl_syscalls.h
    
    * Update nacl_syscall_common.c
    
    ---------
    
    Co-authored-by: lind <lind@nyu.edu>

--- src/shared/platform/lind_platform.h
+++ src/shared/platform/lind_platform.h
@@ -125,7 +125,6 @@
 #define LIND_safe_net_getpeername       145
 #define LIND_safe_net_getifaddrs        146
 
-#define LIND_safe_fs_mknod              160
 #define LIND_safe_fs_fchdir		161
 
 union RustArg {
@@ -182,7 +181,6 @@ int lind_chmod (const char *path, int mode, int cageid);
 int lind_fchmod (int fd, int mode, int cageid);
 
 int lind_fchdir(int fd, int cageid);
-int lind_mknod (const char *path, int mode, unsigned long long dev, int cageid);
 
 int lind_xstat (const char *path, struct lind_stat *buf, int cageid);
 int lind_open (const char *path, int flags, int mode, int cageid);
