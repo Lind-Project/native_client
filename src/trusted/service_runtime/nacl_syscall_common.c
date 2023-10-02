73eb96ef8492e29417931439376fe99cc0f5223d
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-10-01 23:33:43 -0400

    update check

--- src/trusted/service_runtime/nacl_syscall_common.c
+++ src/trusted/service_runtime/nacl_syscall_common.c
@@ -2364,11 +2364,13 @@ int32_t NaClSysShmctl (struct NaClAppThread        *natp,
       return -NACL_ABI_EFAULT;
     }
     bufsysaddr = (struct lind_shmid_ds*) NaClUserToSysAddrRangeProt(nap, (uintptr_t) buf, sizeof(*bufsysaddr), NACL_ABI_PROT_READ);
-    if ((void*) kNaClBadAddress == bufsysaddr) {
+    
+  } else bufsysaddr = NULL;
+
+  if ((void*) kNaClBadAddress == bufsysaddr) {
       NaClLog(2, "NaClSysShmCtl could not translate buffer address, returning %d\n", -NACL_ABI_EFAULT);
       return -NACL_ABI_EFAULT;
-    }
-  } else bufsysaddr = NULL;
+  }
 
   retval = lind_shmctl(shmid, cmd, bufsysaddr, nap->cage_id);
 
