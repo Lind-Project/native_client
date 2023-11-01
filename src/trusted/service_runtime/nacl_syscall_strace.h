989aa23c5dc06ff1ed9cdd47cd56ddf57a62ff1e
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2023-11-01 14:58:58 -0400

    update

--- src/trusted/service_runtime/nacl_syscall_strace.h
+++ src/trusted/service_runtime/nacl_syscall_strace.h
@@ -1,9 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 
-#include <sys/shm.h>
-EXTERN_C_BEGIN
+
 void NaClStraceGetpid(int pid);
 void NaClStraceOpen(char* path, int flags, int mode, int fd);
 void NaClStraceClose(int d, int ret);
-EXTERN_C_END
