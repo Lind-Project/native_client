32ad22562bed37345a4ab0d729ea90f021f2f169
Rupesh Koushik <rupeshkoushik@Rupeshs-Mac-mini.local>
2023-12-31 03:35:38 +0530

    add missing strace fun

--- src/trusted/service_runtime/nacl_syscall_common.c
+++ src/trusted/service_runtime/nacl_syscall_common.c
@@ -1317,7 +1317,7 @@ int32_t NaClSysGetcwd(struct NaClAppThread *natp,
   NaClLog(2, "NaClSysGetcwd: returning %d\n", retval);
 
   #ifdef TRACING
-  NaClStraceGetcwd(nap->cage_id, sysaddr, size, retval);
+  NaClStraceGetcwd(nap->cage_id, (char *)sysaddr, size, retval);
   #endif
 
   return retval;
