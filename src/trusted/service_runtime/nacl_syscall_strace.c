93e4d094f738bb0a14b46ef32a35f5849d874fbe
Rupesh Koushik <rupeshkoushik@Rupeshs-Mac-mini.local>
2024-01-28 00:18:19 +0530

    update mkdir strace

--- src/trusted/service_runtime/nacl_syscall_strace.c
+++ src/trusted/service_runtime/nacl_syscall_strace.c
@@ -179,6 +179,9 @@ void NaClStraceLStat(int cageid, char* path, uintptr_t result, int32_t retval) {
 void NaClStraceMkdir(int cageid, char* path, int mode, int32_t retval) {
 #ifdef TRACING_DASHC
     long long startTime = gettimens();
+    if (retval < 0) { // Assuming negative retval indicates an error
+        syscallStats[SYS_MKDIR].errorCount++;
+    }
 
     // ... original functionality ...
 
@@ -196,13 +199,17 @@ void NaClStraceMkdir(int cageid, char* path, int mode, int32_t retval) {
                          : 0.0;
 
     fprintf(tracingOutputFile, 
-            "%% time     seconds  usecs/call     calls    syscall errors\n");
+            "%% time     seconds  usecs/call     calls    errors syscall\n");
     fprintf(tracingOutputFile, 
             "------ ----------- ----------- --------- --------- ----------------\n");
-    fprintf(tracingOutputFile, 
-            "%.2f    %.9f   %lld        %lld        %s %d\n", 
-            percentTime, totalTimeInSeconds, avgTimeInMicroseconds, 
-            syscallStats[SYS_MKDIR].count, "mkdir", 0);
+    // fprintf(tracingOutputFile, 
+    //         "%.2f    %.9f   %lld        %lld        %d %s\n", 
+    //         percentTime, totalTimeInSeconds, avgTimeInMicroseconds, 
+    //         syscallStats[SYS_MKDIR].count, 0 /* or error count */, "mkdir");
+    fprintf(tracingOutputFile, "%.2f    %.9f   %lld        %lld        %lld %s\n", 
+        percentTime, totalTimeInSeconds, avgTimeInMicroseconds, 
+        syscallStats[SYS_MKDIR].count, syscallStats[SYS_MKDIR].errorCount, "mkdir");
+
 #endif
 
     fprintf(tracingOutputFile, "%d mkdir(%s, %d) = %d\n", cageid, path, mode, retval);
