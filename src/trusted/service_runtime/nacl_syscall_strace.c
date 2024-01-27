9265fe2924892f924a54cb5ed3211ca7421a232f
Rupesh Koushik <rupeshkoushik@Rupeshs-Mac-mini.local>
2024-01-28 00:19:31 +0530

    update mkdir strace

--- src/trusted/service_runtime/nacl_syscall_strace.c
+++ src/trusted/service_runtime/nacl_syscall_strace.c
@@ -202,13 +202,13 @@ void NaClStraceMkdir(int cageid, char* path, int mode, int32_t retval) {
             "%% time     seconds  usecs/call     calls    errors syscall\n");
     fprintf(tracingOutputFile, 
             "------ ----------- ----------- --------- --------- ----------------\n");
-    // fprintf(tracingOutputFile, 
-    //         "%.2f    %.9f   %lld        %lld        %d %s\n", 
-    //         percentTime, totalTimeInSeconds, avgTimeInMicroseconds, 
-    //         syscallStats[SYS_MKDIR].count, 0 /* or error count */, "mkdir");
-    fprintf(tracingOutputFile, "%.2f    %.9f   %lld        %lld        %lld %s\n", 
-        percentTime, totalTimeInSeconds, avgTimeInMicroseconds, 
-        syscallStats[SYS_MKDIR].count, syscallStats[SYS_MKDIR].errorCount, "mkdir");
+    fprintf(tracingOutputFile, 
+            "%.2f    %.9f   %lld        %lld        %d %s\n", 
+            percentTime, totalTimeInSeconds, avgTimeInMicroseconds, 
+            syscallStats[SYS_MKDIR].count, 0 /* or error count */, "mkdir");
+    // fprintf(tracingOutputFile, "%.2f    %.9f   %lld        %lld        %lld %s\n", 
+    //     percentTime, totalTimeInSeconds, avgTimeInMicroseconds, 
+    //     syscallStats[SYS_MKDIR].count, syscallStats[SYS_MKDIR].errorCount, "mkdir");
 
 #endif
 
