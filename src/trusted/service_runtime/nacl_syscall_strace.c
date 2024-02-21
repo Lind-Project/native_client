7517f5607089b51d3e440ffa58964f8cd827421e
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2024-02-21 11:15:45 +0530

    sort

--- src/trusted/service_runtime/nacl_syscall_strace.c
+++ src/trusted/service_runtime/nacl_syscall_strace.c
@@ -772,7 +772,7 @@ void printFinalSyscallStats() {
 
     fprintf(tracingOutputFile, "%% time     seconds  usecs/call     calls    errors ");
     fprintf(tracingOutputFile, "%-*s\n", maxSyscallNameLength, "syscall");
-    fprintf(tracingOutputFile, "------ ----------- ----------- --------- ---------\n");
+    fprintf(tracingOutputFile, "------ ----------- ----------- --------- --------- ");
     for (int i = 0; i < maxSyscallNameLength; i++) fprintf(tracingOutputFile, "-");
     fprintf(tracingOutputFile, "\n");
 
@@ -799,7 +799,7 @@ void printFinalSyscallStats() {
 
     // Print the total summary line
     fprintf(tracingOutputFile, "------ ----------- ----------- --------- --------- ----------------\n");
-    fprintf(tracingOutputFile, "100.00 %.9f          0        %lld      %lld            total\n",
+    fprintf(tracingOutputFile, "100.00 %.9f          0        %lld        %lld            total\n",
       totalSeconds, totalCalls, totalErrors, "total");
   }
 }
