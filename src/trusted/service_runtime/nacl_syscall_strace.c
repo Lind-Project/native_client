13086103d6dbcf25aff60f6c829c1bbe3b936870
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2024-02-21 13:03:04 +0530

    update

--- src/trusted/service_runtime/nacl_syscall_strace.c
+++ src/trusted/service_runtime/nacl_syscall_strace.c
@@ -798,7 +798,7 @@ void printFinalSyscallStats() {
 
     // Print the total summary line
     fprintf(tracingOutputFile, "------ ----------- ----------- --------- --------- ----------------\n");
-    fprintf(tracingOutputFile, "100.00 %.9f          0        %lld        %lld     total\n",
+    fprintf(tracingOutputFile, "100.00 %.9f          0       %lld       %lld     total\n",
       totalSeconds, totalCalls, totalErrors);
   }
 }
