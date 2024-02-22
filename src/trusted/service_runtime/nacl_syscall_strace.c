b0fe488429664043a716b0609f668753efe49cca
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2024-02-22 09:47:03 +0530

    update sort

--- src/trusted/service_runtime/nacl_syscall_strace.c
+++ src/trusted/service_runtime/nacl_syscall_strace.c
@@ -804,7 +804,7 @@ void printFinalSyscallStats() {
         // Print each syscall's stats to the tracing output file
         for (int i = 0; i < validCount; i++) {
             int idx = syscallTimes[i].index;
-            fprintf(tracingOutputFile, "%05.2f  %0.9f   %11lld %9lld %9lld  %s\n",
+            fprintf(tracingOutputFile, "%05.2f  %0.9f   %6lld %9lld %8lld  %s\n",
                    syscallTimes[i].percentTime,
                    (double)syscallStats[idx].totalTime / 1000000000.0,
                    syscallStats[idx].count > 0 ? syscallStats[idx].totalTime / syscallStats[idx].count / 1000 : 0, // Calculate usecs/call
