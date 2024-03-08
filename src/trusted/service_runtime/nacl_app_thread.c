1977c24bb802fca677350f94b91a3683c20c81bd
Nicholas Renner <nicholassrenner@gmail.com>
2024-03-08 00:44:16 -0500

    fix prog ctr

--- src/trusted/service_runtime/nacl_app_thread.c
+++ src/trusted/service_runtime/nacl_app_thread.c
@@ -261,6 +261,7 @@ void WINAPI NaClAppThreadLauncher(void *state) {
 
   lindsetthreadkill(natp->nap->cage_id, natp->host_thread.tid, false); //set up kill table in rustposix
 
+  natp->nap->parent->parent_forking = 0; //reset parent fork
   /*
   * After this NaClAppThreadSetSuspendState() call, we should not
   * claim any mutexes, otherwise we risk deadlock.
@@ -407,6 +408,8 @@ void NaClAppThreadTeardownInner(struct NaClAppThread *natp, bool active_thread) 
   if (last_thread) {
     // we have to wait for NaClWaitForThreadToExit on Main/Exec
     if (nap->tl_type!=THREAD_LAUNCH_FORK) NaClXCondVarWait(&nap->exit_cv, &nap->exit_mu);
+    while(nap->parent_forking); //wait until we escape fork    while(forking);
+
     NaClAppDtor(nap);
     natp->nap = NULL;
   }
@@ -545,6 +548,8 @@ void NaClForkThreadContextSetup(struct NaClAppThread     *natp_parent,
     natp_child->user.r15 = nap_child->mem_start;
     natp_child->user.rsp = (uintptr_t)stack_ptr_child + stack_ptr_offset;
     natp_child->user.rbp = (uintptr_t)stack_ptr_child + base_ptr_offset;
+    natp_child->user.prog_ctr = (natp_child->user.prog_ctr & 0xffffffff) + nap_child->mem_start;
+    natp_child->user.new_prog_ctr = (natp_child->user.prog_ctr & 0xffffffff) + nap_child->mem_start;
     natp_child->user.sysret = 0;
 
   /* examine arbitrary stack values */
@@ -604,6 +609,8 @@ int NaClAppThreadSpawn(struct NaClAppThread     *natp_parent,
   if (tl_type == THREAD_LAUNCH_FORK) {
     nap_parent = natp_parent->nap;
     if (!nap_parent->running) return 0;
+
+    nap_parent->parent_forking = 1;
 
     NaClXMutexLock(&nap_parent->mu);
     NaClXMutexLock(&nap_child->mu);
